63e57d7dbc14155119befb15d4877bf9f25996ae
suzuki-n <vee00703@nifty.ne.jp>
2014-05-24 18:45:06 +0900

    奈良市版機能(隔週対応・年末年始 skip)反映

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ var AreaModel = function() {
   this.trash = new Array();
   /**
   各ゴミのカテゴリに対して、最も直近の日付を計算します。
-*/
+  */
   this.calcMostRect = function() {
     for (var i = 0; i < this.trash.length; i++) {
       this.trash[i].calcMostRect(this);
@@ -41,7 +41,7 @@ var AreaModel = function() {
   }
   /**
   ゴミのカテゴリのソートを行います。
-*/
+  */
   this.sortTrash = function() {
     this.trash.sort(function(a, b) {
       var at = a.mostRecent.getTime();
@@ -86,6 +86,12 @@ var TrashModel = function(_lable, _cell, remarks) {
     } else if (this.dayCell[j].length == 2 && this.dayCell[j].substr(0,1) != "*") {
       result_text += "第" + this.dayCell[j].charAt(1) + this.dayCell[j].charAt(0) + "曜日 ";
     } else if (this.dayCell[j].length == 2 && this.dayCell[j].substr(0,1) == "*") {
+    } else if (this.dayCell[j].length == 10 && this.dayCell[j].substr(0,1) == "隔") {
+      /**** MOD: PICK biweek, Ex:隔月20140401 ****/
+      /****ADD****/
+      result_text += "隔週" + this.dayCell[j].charAt(1) + "曜 ";
+      this.regularFlg = 2;      // 隔週フラグ
+      /****ADD****/
     } else {
       // 不定期回収の場合（YYYYMMDD指定）
       result_text = "不定期 ";
@@ -165,15 +171,22 @@ var TrashModel = function(_lable, _cell, remarks) {
             d.setTime(date.getTime() + 1000 * 60 * 60 * 24 *
               ((7 + getDayIndex(day_mix[j].charAt(0)) - date.getDay()) % 7) + week * 7 * 24 * 60 * 60 * 1000
             );
+            //年末年始休暇のスキップ対応
+            if (SkipSuspend) {
+              if (areaObj.isBlankDay(d)) {
+		continue;
+	      }
+	    }
             //年末年始のずらしの対応
             //休止期間なら、今後の日程を１週間ずらす
-            if (areaObj.isBlankDay(d)) {
-              if (WeekShift) {
+	    if (areaObj.isBlankDay(d)) {
+	      if (WeekShift) {
                 isShift = true;
               } else {
                 continue;
               }
             }
+	    ////
             if (isShift) {
               d.setTime(d.getTime() + 7 * 24 * 60 * 60 * 1000);
             }
@@ -192,6 +205,31 @@ var TrashModel = function(_lable, _cell, remarks) {
           }
         }
       }
+      /****ASS****/
+    } else if (this.regularFlg == 2) {
+      // 隔週回収の場合は、basedateに指定初回日付をセット
+      for (var j in day_mix) {
+        var year = parseInt(day_mix[j].substr(2, 4));
+        var month = parseInt(day_mix[j].substr(6, 2)) - 1;
+        var day = parseInt(day_mix[j].substr(8, 2));
+        var basedate = new Date(year, month, day);
+
+	//week=0が第1週目です。
+	for (var week = 0; week < 27; week++) {
+          // basedate を起点に、最も近い偶数週目を計算する。
+          var d = new Date(date);
+          // basedate を基準に、最大53週まで増加させて隔週を表現
+          d.setTime( basedate.getTime() + week * 14 * 24 * 60 * 60 * 1000 );
+          //年末年始休暇のスキップ対応
+          if (SkipSuspend) {
+            if (areaObj.isBlankDay(d)) {
+	      continue;
+	    }
+          }
+	  day_list.push(d);
+	}
+      }
+    /***ADD*****/   
     } else {
       // 不定期回収の場合は、そのまま指定された日付をセットする
       for (var j in day_mix) {
@@ -268,7 +306,7 @@ var DescriptionModel = function(data) {
  * target.csvのモデルです。
  */
 var TargetRowModel = function(data) {
-  this.type = data[0];
+  this.label = data[0];
   this.name = data[1];
   this.notice = data[2];
   this.furigana = data[3];
@@ -405,7 +443,7 @@ $(function() {
           var row = new TargetRowModel(data[i]);
           for (var j = 0; j < descriptions.length; j++) {
             //一致してるものに追加する。
-            if (descriptions[j].label == row.type) {
+            if (descriptions[j].label == row.label) {
               descriptions[j].targets.push(row);
               break;
             }
