5e0b3ca0f60f50a04b7faf214d7a92d5f12aad42
suzuki-n <vee00703@nifty.ne.jp>
2014-08-24 13:48:33 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -81,6 +81,9 @@ var TrashModel = function(_lable, _cell, remarks) {
   var today = new Date();
 
   for (var j in this.dayCell) {
+
+    if ( this.dayCell[j] == "--" ) continue;
+
     if (this.dayCell[j].length == 1) {
       result_text += "毎週" + this.dayCell[j] + "曜日 ";
     } else if (this.dayCell[j].length == 2 && this.dayCell[j].substr(0,1) != "*") {
