d887c2ef1f7c77c39e37a9474c23ad927571a03b
suzuki-n <vee00703@nifty.ne.jp>
2014-10-26 19:23:05 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -7,7 +7,7 @@ var SVGLabel = true; // SVGイメージを使用するときは、true。用意�
 
 var MaxDescription = 9; // ごみの最大種類、９を超えない場合は変更の必要はありません。
 
-var MaxMonth = 5;
+var MaxMonth = 16;
 
 var WeekShift = false; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
 
